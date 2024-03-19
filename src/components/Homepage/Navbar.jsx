dd22e37537d14ca58185d0f051c749123a5441a5
vikasvishwakarma963 <vikasvishwakarma963@gmail.com>
2024-03-19 10:04:02 +0000

    23

--- src/components/Homepage/Navbar.jsx
+++ src/components/Homepage/Navbar.jsx
@@ -23,13 +23,15 @@ const Navbar = () => {
     setOpenNavigation(!openNavigation);
   };
   return (
-    <div className="fixed h-full top-0  border-b border-gray-200/20 z-[100]  w-full  ">
+    <div className={`fixed  lg:backdrop-blur-md top-0  lg:${()=>toggle()} border-b border-gray-200/20 z-[100]  w-full ${
+      openNavigation ? " bg-gradient-to-r from-blue-950  to-black" : " backdrop-blur-md"
+    }  `}>
       <div className="flex  mx-auto  items-center justify-between px-3  lg:px-7   ">
         <a href="#master" className="block w-[12rem ] flex items-center  xl:mr-8 ">
           <img src={image} className="lg:w-[200px]  w-[140px] "  alt="MASTER" />
         </a>
-        <nav
-          className={`${openNavigation ? "flex  bg-gradient-to-br from-blue-950 via-gray-950 to-black" : "hidden"
+        {/* <nav
+          className={`${openNavigation ? "flex  " : "hidden"
             }  fixed top-[55px] left-0 right-0 bottom-0  lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div className=" z-2 flex flex-col text-2xl items-center justify-center m-auto lg:flex-row ">
@@ -45,6 +47,28 @@ const Navbar = () => {
               </a>
             ))}
           </div>
+        </nav> */}
+        <nav
+          className={`${
+            openNavigation ? "flex overflow-y-scroll bg-gradient-to-br from-blue-950 via-gray-950 to-black" : "hidden"
+          } fixed top-[55px]    left-0 right-0 bottom-0  lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+        >
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+            {navigation.map((item) => (
+              <a
+                key={item.id}
+                href={item.url}
+                onClick={handleClick}
+                className={`block relative font-bold text-2xl uppercase text-gray-500 transition-colors lg:hover:cursor-pointer ${
+                  item.onlyMobile ? "lg:hidden" : ""
+                } px-6 py-6 md:py-4  lg:text-xl lg:font-bold  lg:leading-5 lg:hover:text-white xl:px-12`}
+              >
+                {item.title}
+              </a>
+            ))}
+          </div>
+
+         
         </nav>
 
         <div className="flex gap-4 font-bold items-center py-3 ">
