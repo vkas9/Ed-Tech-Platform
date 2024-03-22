8ed1a7faf889a4098634126a636878bc75fc6e19
vikasvishwakarma963 <vikasvishwakarma963@gmail.com>
2024-03-22 11:28:09 +0000

    3

--- src/Pages/Home/SkillSection.jsx
+++ src/Pages/Home/SkillSection.jsx
@@ -5,25 +5,25 @@ import techimage from "../../assets/tech.png";
 
 const SkillSection = () => {
     return (
-        <div className="relative container mt-[4rem] min-h-[1200px] mx-auto px-[1rem] flex flex-col gap-7  lg:px-[4rem] md:flex-row">
-            <div className=" z-10  max-w-[30em]  ">
+        <div className="relative container mt-[4rem]  mx-auto px-[1rem] flex flex-col gap-7  lg:px-[4rem] md:flex-row">
+            <div className=" z-10 text-center sm:text-start max-w-[30em]  ">
                 <h1 className=" text-3xl lg:text-5xl font-bold mb-4 md:mb-10 ">AI Chat App for seamless collaboration</h1>
-                <ul className=" text-xl lg:text-2xl mb-11 md:mb-[5rem] max-w-[20rem]">
+                <ul className=" text-xl lg:text-2xl  mx-auto text-center sm:text-start mb-11 md:mb-[5rem] max-w-[20rem]">
                     <li className="mb-3 ">
-                        <div className="flex items-center mb-2 gap-5">
+                        <div className="flex justify-center sm:justify-start items-center mb-2 gap-5">
                             <FaHandPointRight className="text-purple-500"/>
                             <h6>Seamless Integration</h6>
                         </div>
                         <p className="text-sm text-gray-300">With smart automation and top-notch security, it's the perfect solution for teams looking to work smarter.</p>
                     </li>
                     <li className="mb-3 ">
-                        <div className="flex items-center mb-2 gap-5">
+                        <div className="flex justify-center sm:justify-start items-center mb-2 gap-5">
                             <FaHandPointRight className="text-purple-500"/>
                             <h6>Smart Automation</h6>
                         </div>
                     </li>
                     <li className="mb-3 ">
-                        <div className="flex items-center mb-2 gap-5">
+                        <div className="flex justify-center sm:justify-start items-center mb-2 gap-5">
                             <FaHandPointRight className="text-purple-500"/>
                             <h6>Top-notch Security</h6>
                         </div>
