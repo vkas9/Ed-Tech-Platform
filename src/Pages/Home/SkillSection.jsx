3f35b72ac7e7f20ce198d4650a09f336b502ef2e
Vikas <81951052+vkas9@users.noreply.github.com>
2024-03-22 18:00:26 +0530

    Update SkillSection.jsx

--- src/Pages/Home/SkillSection.jsx
+++ src/Pages/Home/SkillSection.jsx
@@ -7,7 +7,7 @@ const SkillSection = () => {
     return (
         <div className="relative container mt-[4rem]  mx-auto px-[1rem] flex flex-col gap-7  lg:px-[4rem] md:flex-row">
             <div className=" z-10 text-center mx-auto sm:text-start max-w-[30em]  ">
-                <h1 className=" text-3xl lg:text-5xl font-bold mb-4 md:mb-10 ">AI Chat App for seamless collaboration</h1>
+                <h1 className=" text-3xl lg:text-5xl text-center font-bold mb-4 md:mb-10 ">AI Chat App for seamless collaboration</h1>
                 <ul className=" text-xl lg:text-2xl  mx-auto text-center sm:text-start mb-11 md:mb-[5rem] max-w-[20rem]">
                     <li className="mb-3 ">
                         <div className="flex justify-center sm:justify-start items-center mb-2 gap-5">
