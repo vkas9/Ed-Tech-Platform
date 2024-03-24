38c20433b1c58df82dafe614efaa04b49cfc9bbd
Vikas Vishwakarma <vikasvishwakarma1649@gmail.com>
2024-03-25 01:25:08 +0530

    Footer path changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./Pages/Home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Homepage/Navbar";
+import Footer from "./Pages/Home/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
         </Routes>
+        <Footer/>
       </Router>
     </div>
   );
--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Footer=()=>{
     return (
-        <div className=" hidden md:flex  font-semibold text-gray-200 px-4 py-4 bg-gradient-to-t from-blue-950 to-transparent mt-[10rem] flex items-center justify-between ">
+        <div className=" hidden md:flex  font-semibold text-gray-200 px-4 py-4 bg-gradient-to-t from-blue-950 to-transparent mt-[10rem]  items-center justify-between ">
             <div>
                 <h3>© 2024 All rights reserved</h3>
             </div>
