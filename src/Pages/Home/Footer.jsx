1cd66615cfae992c43c0f3237de2ead72a372ed5
Vikas Vishwakarma <vikasvishwakarma1649@gmail.com>
2024-03-26 03:48:17 +0530

    reset Formatting

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -1,18 +1,17 @@
 import { Link } from "react-router-dom";
 
-const Footer=()=>{
-    return (
-        <div className=" hidden md:flex  font-semibold text-gray-200 px-4 py-4 bg-gradient-to-t from-blue-950 to-transparent mt-[10rem]  items-center justify-between ">
-            <div>
-                <h3>© 2024 All rights reserved</h3>
-            </div>
-            <div className=" flex gap-3">
-                <Link>Privacy Policy</Link>
-                <Link>Terms & Conditions</Link>
-                <Link>Terms of Use</Link>
-            </div>
-
-        </div>
-    );
-}
+const Footer = () => {
+  return (
+    <div className=" hidden md:flex  font-semibold text-gray-200 px-4 py-4 bg-gradient-to-t from-blue-950 to-transparent mt-[10rem]  items-center justify-between ">
+      <div>
+        <h3>© 2024 All rights reserved</h3>
+      </div>
+      <div className=" flex gap-3">
+        <Link>Privacy Policy</Link>
+        <Link>Terms & Conditions</Link>
+        <Link>Terms of Use</Link>
+      </div>
+    </div>
+  );
+};
 export default Footer;
