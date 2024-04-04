d16c1d18f0acafd8e9606e5e757fbdb5dad4f6f4
Vikas Vishwakarma <vikasvishwakarma963@gmail.com>
2024-04-04 22:15:21 +0530

    github link added

--- src/Pages/Home/AboutUs.jsx
+++ src/Pages/Home/AboutUs.jsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import video from "../../assets/particle.mp4";
 import Button from "../../components/Homepage/Button";
+import toast from "react-hot-toast";
 const AboutUs = () => {
+  const handleClick=()=>{
+    console.log("hi");
+    toast.success("success")
+  }
   return (
     <div className=" overflow-hidden relative py-[5rem]  flex flex-col items-center mx-auto ">
       <div className=" relative  z-[3] py-4 gap-2 flex flex-col text-center h-full ">
@@ -9,7 +14,7 @@ const AboutUs = () => {
         <h1 className=" text-6xl px-1 md:text-6xl font-bold">
           Learn. Grow. Succeed.
         </h1>
-        <Link to={"/signup"} className=" mt-5">
+        <Link to={"/signup"} onClick={handleClick} className=" mt-5">
           <Button className={"mx-auto bg-yellow-500 outline-none py-2"}>
             Explore More
           </Button>
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import Store from "./store.jsx/store.js";
 import { Provider } from "react-redux";
-import { Toaster } from "react-hot-toast";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={Store}>
     <React.StrictMode>
       <App />
-      <Toaster/>
+     
     </React.StrictMode>
   </Provider>
 );
