19e2e25ee33d37bb5b5f4437aae067a69a57bf3b
Vikas Vishwakarma <vikasvishwakarma963@gmail.com>
2024-04-05 19:53:18 +0530

    github link added

--- src/components/core/auth/LoginForm.jsx
+++ src/components/core/auth/LoginForm.jsx
@@ -4,14 +4,16 @@ const LoginForm=()=>{
     const handle=()=>{
         toast.success("Successfull login!")
     }
+
+    
     return (
         <div className=" h-screen flex  items-center justify-center w-fit     mx-auto ">
             <Formik  onSubmit={(values)=>{console.log(values)}} initialValues={{email:"",password:""}} >
                 <Form className="flex  flex-col ">
                     <label className="text-xl">Email Address</label>
-                    <Field  className=" text-black outline-none p-3 w-full md:w-[25rem] rounded-md font-semibold text-xl" placeholder="Enter email address" name="email" type="email" />
+                    <Field  required className=" text-black focus:outline-green-500 outline-none p-3 w-full md:w-[25rem] rounded-md font-semibold text-xl" placeholder="Enter email address" name="email" type="email" />
                     <label className="text-xl mt-6 ">Password</label>
-                    <Field className="text-black p-3 w-full md:w-[25rem] outline-none rounded-md text-xl font-semibold" placeholder="Enter password" name="password" type="password" />
+                    <Field required className="text-black p-3 focus:outline-green-500  w-full md:w-[25rem] outline-none rounded-md text-xl font-semibold" placeholder="Enter password" name="password" type="password" />
                     
                     <button onClick={handle}  className="bg-yellow-500 w-full text-black font-bold text-xl active:bg-yellow-400 p-2 rounded-md mt-3  " type="submit">Login</button>
                 </Form>
