dfa0f837ba8dce272b62edf4057eb53313aa6043
Vikas Vishwakarma <vikasvishwakarma963@gmail.com>
2024-04-07 23:53:03 +0530

    screen changed

--- src/Pages/Home/Pricing.jsx
+++ src/Pages/Home/Pricing.jsx
@@ -2,7 +2,7 @@ import PricingCard from "../../components/Homepage/PricingCard";
 import HightlightText from "../../components/Homepage/HightlightText";
 
 const Pricing = () => {
-  return <div className="container relative mx-auto  mt-[6rem] px-2 mb-5 lg:px-4 lg:mt-[8rem] ">
+  return <div className=" relative mx-auto  mt-[6rem] px-2 mb-5 lg:px-4 lg:mt-[8rem] ">
         <div className="max-w-[1100px] w-full  mx-auto">
             <h1 className=" py-3 font-extrabold mb-[5rem] text-4xl lg:text-5xl 2xl:text-6xl text-center bg-gradient-to-br from-gray-400 to-white bg-clip-text text-transparent">Become the Master of High Demanding Technology </h1>
         </div>
