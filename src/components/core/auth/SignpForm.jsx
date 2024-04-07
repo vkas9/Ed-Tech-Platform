1f059cc0fe1ce3d853bbe12528accf1e9057678f
Vikas Vishwakarma <vikasvishwakarma963@gmail.com>
2024-04-07 23:39:05 +0530

    auth updated

--- src/components/core/auth/SignpForm.jsx
+++ src/components/core/auth/SignpForm.jsx
@@ -17,8 +17,8 @@ const SignupForm = () => {
    
   };
   return (
-    <div className="flex min-h-[calc(100vh-2.8rem)] flex-col gap-3 px-2 items-center justify-center">
-    <h1 className="text-[2.5rem]  overflow-hidden mx-2 mt-[4rem] xs:mt-[0rem] md:text-[4em]  bg-gradient-to-r from-red-500 via-purple-400 to-blue-500 bg-clip-text text-transparent font-bold text-center ">MASTER WORLD</h1>
+    <div className="flex min-h-[calc(100vh-2.8rem)] flex-col gap-1 xs:gap-3 px-2 items-center justify-center">
+    <h1 className="text-[2.5rem]  overflow-hidden mt-[1rem] mx-2 xs:-mt-[2rem] sm:-mt-[1rem] md:text-[4em]  bg-gradient-to-r from-red-500 via-purple-400 to-blue-500 bg-clip-text text-transparent font-bold text-center ">MASTER WORLD</h1>
       <Formik
         onSubmit={(values) => handleSubmit(values)}
         initialValues={{
@@ -32,9 +32,9 @@ const SignupForm = () => {
         }}
       >
         <Form>
-          <div className=" flex flex-col px-2 gap-5">
+          <div className=" flex flex-col px-2 gap-2 xs:gap-5">
          
-            <div className="flex w-full flex-col xs:flex-row justify-center gap-5">
+            <div className="flex w-full flex-col xs:flex-row justify-center gap-2 xs:gap-2">
               <div >
                 
                 
@@ -56,7 +56,7 @@ const SignupForm = () => {
            
             <Field className=" bg-gray-700  outline-none p-3 w-full rounded-md font-semibold text-sm sm:text-xl" placeholder="Enter Contact Number" required name="Contact_Number" type="tel" max={10}></Field>
             </div>
-            <div className="flex flex-col xs:flex-row justify-center gap-5">
+            <div className="flex flex-col xs:flex-row justify-center gap-2 xs:gap-2">
               <div>
                 
                
