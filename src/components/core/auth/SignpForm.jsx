c680e8417fd6e2abdf4b2edc4c6bf06f6ca0b6d8
Vikas Vishwakarma <vikasvishwakarma963@gmail.com>
2024-04-09 18:33:02 +0530

    df

--- src/components/core/auth/SignpForm.jsx
+++ src/components/core/auth/SignpForm.jsx
@@ -17,8 +17,8 @@ const SignupForm = () => {
    
   };
   return (
-    <div className="flex min-h-[calc(100vh-2.8rem)] flex-col gap-1 xs:gap-3 px-2 items-center justify-center">
-    <h1 className="text-[2.5rem]  overflow-hidden mt-[1rem] mx-2 xs:-mt-[2rem] sm:-mt-[1rem] md:text-[4em]  bg-gradient-to-r from-red-500 via-purple-400 to-blue-500 bg-clip-text text-transparent font-bold text-center ">Create Student Account</h1>
+    <div className="flex  min-h-[calc(100vh-2.8rem)]  pt-[3rem] sm:pt-[5rem] md:pt-[10rem] flex-col gap-1 xs:gap-3 px-2 items-center ">
+    <h1 className="text-[2.5rem]  overflow-hidden mt-[1rem] mx-2  md:text-[4em]  bg-gradient-to-r from-red-500 via-purple-400 to-blue-500 bg-clip-text text-transparent font-bold text-center ">Create Student Account</h1>
       <Formik
         onSubmit={(values) => handleSubmit(values)}
         initialValues={{
@@ -31,8 +31,8 @@ const SignupForm = () => {
           role: "",
         }}
       >
-        <Form>
-          <div className=" flex flex-col px-2 gap-2 xs:gap-5">
+        <Form className=" ">
+          <div className="   border border-gray-500/50  w-screen xs:w-full p-4 rounded-xl flex flex-col px-2 gap-2 xs:gap-5">
          
             <div className="flex w-full flex-col xs:flex-row justify-center gap-2 xs:gap-2">
               <div >
@@ -56,7 +56,7 @@ const SignupForm = () => {
            
             <Field className=" bg-gray-700  outline-none p-3 w-full rounded-md font-semibold text-sm sm:text-xl" placeholder="Enter Contact Number" required name="Contact_Number" type="tel" max={10}></Field>
             </div>
-            <div className="flex flex-col vm:flex-row justify-center gap-5 vm:gap-2">
+            <div className="flex flex-col vm:flex-row justify-center gap-2 xs:gap-5 vm:gap-2">
               <div>
                 
                
