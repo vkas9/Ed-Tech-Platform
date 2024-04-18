5d9f1c0b37b15f97e94a22cf83aac931f6f9ef5e
Vikas Vishwakarma <vikasvishwakarma963@gmail.com>
2024-04-18 12:53:36 +0530

    host changed

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/v1": "https://edback-dsur.onrender.com/",
+      "/api/v1": "https://edback-dsur.onrender.com",
     },
   },
 });
