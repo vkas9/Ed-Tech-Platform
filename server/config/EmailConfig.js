1ee13fa9704a5095e6168bff1c8a8def0945d393
Vikas Vishwakarma <vikasvishwakarma963@gmail.com>
2024-05-18 02:53:02 +0530

    email config changed

--- server/config/EmailConfig.js
+++ server/config/EmailConfig.js
@@ -5,7 +5,7 @@ const nodemailer=require("nodemailer");
 const transporter= nodemailer.createTransport({
     host:process.env.EMAIL_HOST,
     port:process.env.EMAIL_PORT,
-    secure:false,
+    secure:true,
     auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS
