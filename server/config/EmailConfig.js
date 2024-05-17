f4ece183bb0e49a50d4b40d1270ee2a904a29e00
Vikas Vishwakarma <vikasvishwakarma963@gmail.com>
2024-05-18 03:03:01 +0530

    email config changed

--- server/config/EmailConfig.js
+++ server/config/EmailConfig.js
@@ -10,5 +10,13 @@ const transporter= nodemailer.createTransport({
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS
     }
+    
 })
+transporter.verify((error, success) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Server is ready to take our messages');
+    }
+});
 module.exports=transporter;
