a3e8425ad1092dde1be0c75c744798f2796916a0
Vikas Vishwakarma <vikasvishwakarma963@gmail.com>
2024-06-05 13:15:32 +0530

    confirm model updated

--- src/Pages/Dashboard/ConfirmModal.jsx
+++ src/Pages/Dashboard/ConfirmModal.jsx
@@ -4,15 +4,15 @@ import { motion } from "framer-motion";
 const ConfirmModal=({modalData})=>{
   
     return (
-        <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration:.2,delay:0}}  className="top-0 left-0 z-[9999] flex justify-center bg-black/60 fixed w-[100vw] h-[100vh]  backdrop-blur-md">
-            <div className="fixed flex py-6 flex-col text-center items-center gap-3  w-[400px] top-[35%]   rounded-xl bg-white/10 p-2  ">
-                <span className="text-4xl ">{modalData?.text1}</span>
-                <p className="text-gray-500/80 ">{modalData?.text2}</p>
+        <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration:.2,delay:0}}  className="top-0  left-0 z-[9999] flex justify-center bg-black/60 fixed w-[100vw] h-[100vh]  backdrop-blur-md">
+            <div className="fixed flex py-6 flex-col text-center items-center gap-3  w-[96%]  sm:w-[400px] top-[35%]   rounded-xl bg-white/10 p-2  ">
+                <span className="text-4xl font-semibold">{modalData?.text1}</span>
+                <p className="text-gray-500/80 font-semibold ">{modalData?.text2}</p>
                 <div className=" flex gap-5 ">
                    <IconBtn   
                     onClick={modalData?.btn1Handler}
                     text={modalData?.btn1Text}
-                    className={"bg-gray-400/20 text-2xl text-white/90 py-2  px-2 transition-all duration-100 hover:bg-white/20 rounded-md"}
+                    className={"bg-gray-400/20 text-2xl text-white/90  px-2 transition-all duration-100 hover:bg-white/20 rounded-md"}
                    />
                    <button onClick={modalData?.btn2Handler} className="bg-gray-400/20 text-white/90 text-2xl py-2 px-2 transition-all duration-100 hover:bg-white/20 rounded-md">
                     {modalData?.btn2Text}
