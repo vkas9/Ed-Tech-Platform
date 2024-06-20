30915447853e0944dd1d6ebfa6aae57b28121686
Vikas Vishwakarma <vikasvishwakarma963@gmail.com>
2024-06-21 01:59:12 +0530

    navbar updated

--- src/components/Homepage/Navbar.jsx
+++ src/components/Homepage/Navbar.jsx
@@ -140,7 +140,7 @@ const Navbar = () => {
               : "hidden"
           } fixed top-[70px] md:top-[84px] rounded-t-[2.5rem]  left-0 right-0 bottom-0  lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
-          <div className="relative lg:border-r lg:border-l border-gray-500/20 rounded-full  z-2 flex flex-col select-none lg:select-text items-center justify-center m-auto lg:flex-row">
+          <div className="relative lg:border-r lg:border-l border-gray-500/30 rounded-full  z-2 flex flex-col select-none lg:select-text items-center justify-center m-auto lg:flex-row">
             {navigation.map((item, index) => (
               <div key={index}>
                 {item.title === "Learn" ? (
