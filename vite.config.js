d111af07b8fb71181d993d7b7872e90b0916d90f
Vikas Vishwakarma <vikasvishwakarma963@gmail.com>
2024-06-23 23:27:52 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/beta": {
-        target:"https://ed-tech-platform-1-n5ez.onrender.com",
-        changeOrigin: true,
-        secure: false
-      }
-    },
-  },
+  
 });
